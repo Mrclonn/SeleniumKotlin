b41c621e123101d2bd08cad963ccbf084e2592d2
Mrclonn <tevmgou@gmail.com>
2021-12-28 11:47:41 +0300

    28.12.2021 complete class IeCHS

--- src/main/kotlin/me/tum/Main.kt
+++ src/main/kotlin/me/tum/Main.kt
@@ -5,13 +5,14 @@ const val urlWebClassifier = "http://s-vserver18:81/WebClassifier/"
 const val nameParameter = "param1"
 var countValue = 3
 const val urlCHSTest2 = "http://s-vserver38/TEL.CHS.Web.Test2"
-const val serialNum = "52200007"
+const val serialNum = "07300176"
 
 
 fun main() {
 //    IeClassifier().createNewType()
 //    IeClassifier().createNewTypeWeb()
 //    ChromeClassifier().createNewType()
-    IeCHS().createNewRuClaim()
+//    IeCHS().createNewRuClaim()
+//    IeCHS().createNewMANClaim()
 }
 
